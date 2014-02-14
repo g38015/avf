e7277523d1c1b7b9e02126fe0131f6bf63175a3a
phitchcock <phitchcock@me.com>
2014-02-13 17:00:34 -0800

    working on gh-pages fix

--- project1/platforms/android/bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/Users/peter/avf/project1/platforms/android/bin/AndroidManifest.xml : \
-/Users/peter/avf/project1/platforms/android/AndroidManifest.xml \
--- project1/platforms/android/bin/phoneGapDemo-debug-unaligned.apk.d
+++
@@ -1,4 +0,0 @@
-/Users/peter/avf/project1/platforms/android/bin/phoneGapDemo-debug-unaligned.apk : \
-/Users/peter/avf/project1/platforms/android/bin/phoneGapDemo.ap_ \
-/Users/peter/avf/project1/platforms/android/bin/classes.dex \
-/Users/peter/avf/project1/platforms/android/CordovaLib/bin/classes.jar \
